69ce36a296bc39a6abc0d09a8f81af71865d5fcd
kkeroo <jasko10.kerec@gmail.com>
2021-11-16 21:47:21 +0100

    delujoc genetic algorithm in random search

--- assigment1.r
+++ assigment1.r
@@ -6,18 +6,24 @@ library(GA)
 # - ali moramo porabiti vsa stevila?
 # - kaksen je input? 
 
-numbers_and_operators <- c("10","25","100","5","3","+","-","/","*")
-goal <- 2512
-
+numbers_and_operators <- c("4","25","66","5","3", "13","/", "+","-","*")
+goal <- 1000
+num_numbers = 0
+for (i in 1:length(numbers_and_operators)){
+  if (!is.na(as.numeric(numbers_and_operators[i]))){
+    num_numbers = num_numbers + 1
+  }
+}
+num_operators = length(numbers_and_operators) - num_numbers
+expression_length = 2*num_numbers-1
+num_operators_in_exp = expression_length - num_numbers
 
 myInitPopulation <- function(object) 
 {
   populationSize <- object@popSize
-  n <- seq.int(object@lower, object@upper)
-  n <- length(n)
-  print(n)
-  n <- 9
-  population <- matrix(nrow=populationSize,ncol=n)
+  #n <- seq.int(object@lower, object@upper)
+  #n <- length(n)
+  population <- matrix(nrow=populationSize,ncol=expression_length)
   
   for(i in 1:populationSize) {
     # ustvarimo validno enacbo
@@ -25,13 +31,16 @@ myInitPopulation <- function(object)
     # oblika: S,O,S,O,S,O,S,O,S
     
     # premesamo stevila in vstavimo v enacbo
-    chosenNum <- sample(c(1:5),5)
-    p <- c(1:9)
-    p[c(1,3,5,7,9)] <- chosenNum
-    
+    chosenNum <- sample(c(1:num_numbers),num_numbers)
+    p <- c(1:expression_length)
+    #seq(1,expression_length, by=2)
+    #p[c(1,3,5,7,9)] <- chosenNum
+    p[seq(1, expression_length, by=2)] <- chosenNum
+  
     j <- 2
-    for(x in 1:4) {
-      opIndex <- sample(c(6:9),1)
+    for(x in 1:num_operators_in_exp) {
+      opIndex <- sample(c((num_numbers+1):length(numbers_and_operators)),1)
+      
       p[j] <- opIndex
       j <- j + 2
     }
@@ -53,92 +62,22 @@ myInitPopulation <- function(object)
 myFitness <- function(equation) 
 {
   
-  print("Fitness eq")
-  print(numbers_and_operators[equation])
+  #print("Fitness eq")
+  #print(numbers_and_operators[equation])
   str <- paste(numbers_and_operators[equation], collapse="")
-  print("STR")
-  print(str)
+  #print("STR")
+  #print(str)
   evaluated <- eval(parse(text=str))
 
-  # print(equation)
+  #print("FITNESS")
+  #print(equation)
   # print(str)
-  # print(evaluated)
+  #print(evaluated)
+  #print("-------")
+  difference <- abs(goal - evaluated)
   
-  diff <- abs(goal - evaluated)
-  
-  
-  return(1/(diff + 1))
-}
-
-count <- 0
-
-myCrossover <- function(object,parents)
-{
-  print("ZACETEK CROSSOVERJA")
-  print(numbers_and_operators[object@population])
-  parents <- object@population[parents,,drop = FALSE]
-  n <- length(parents[1,])
-  # Izberemo iz katerega parenta se vzame prvi del in
-  # iz katerega drugi.
-  prvidelParent <- sample(c(1,2),1)
-  drugidelParent <- 3 - prvidelParent
-  ix <- sample(c(2:n-1), 1)
-  child_one <- c(parents[prvidelParent, 1:ix], parents[drugidelParent, (ix+1):n])
-  child_two <- c(parents[drugidelParent, 1:ix], parents[prvidelParent, (ix+1):n])
-  #print("Starsa")
-  #print(parents[1,])
-  #print(parents[2,])
-  #print("Otrok 1")
-  #print(child_one)
-  #print("Otrok 2")
-  #print(child_two)
-  #print("Part one")
-  #print("Eq 1")
-  #print(numbers_and_operators[child_one])
-  #print("Eq 2")
-  #print(numbers_and_operators[child_two])
-  #print(prvidelParent)
-  #print("part two")
-  #print(drugidelParent)
-  #print("ix")
-  #rint(ix)
-  children <- matrix(nrow = 2, ncol = n)
-  children[1,] <- child_one
-  children[2,] <- child_two
-  children <- c(child_one, child_two)
-  #fitness <- c(myFitness(child_one), myFitness(child_two))
-  fitness<- c(1,1)
-  count <- count + 1
-  print("Count")
-  print(count)
-  print("KONEC CROSSOVERJA")
-  #return(list(children=parents, fitness = fitness))
-  return(list(children=children, fitness = rep(NA, 2)))
-  
-}
-
-myCrossover2 <- function(object, parents){
-  print("CROSSOVER")
-  print("population")
-  for (i in 1:object@popSize){
-    print(numbers_and_operators[object@population[i,]])
-  }
-  
-  print(numbers_and_operators[object@population])
-  parents <- object@population[parents,, drop=FALSE]
-  n <- ncol(parents)
-  prvidelStarsa <- sample(c(1,2), 1)
-  drugidelStarsa <- 3 - prvidelStarsa
-  ix <- sample(c(2:n-1), 1)
-  children <- matrix(as.double(NA), nrow=2, ncol=n)
-  children[1,1:ix] <- parents[prvidelStarsa, 1:ix]
-  children[1, (ix+1):n] <- parents[drugidelStarsa, (ix+1):n]
-  children[2,1:ix] <- parents[drugidelStarsa, 1:ix]
-  children[2, (ix+1):n] <- parents[prvidelStarsa, (ix+1):n]
-  
-  out <- list(children=children, fitness = rep(NA,2))
-  print("KONEC CROSSOVERJA")
-  return(out)
+  return(1/(difference + 1))
+  #return(-difference)
 }
 
 cros <- function(object, parents)
@@ -150,20 +89,32 @@ cros <- function(object, parents)
   print("-------POP----------")
   for (i in 1:object@popSize){
     print(numbers_and_operators[object@population[i,]])
-    #nas = is.na()
+    nas = is.na(object@population[i,])
+    ix_nas = which(nas)
   }
   print("---------------------")
-  cxPoints <- sample(1:n, 1)
+  
   children <- matrix(nrow = 2, ncol = n)
   children[1,] <- parents[1,]
   children[2,] <- parents[2,]
+  operators_ixs = seq(2,expression_length, by=2)
+  
+  # število zamenjav
+  cross_iterations = sample(1:num_operators, 1)
+  for (i in 1:cross_iterations) {
+    # ix...indeks operatorja, ki se bo zamenjal
+    ix = sample(1:length(operators_ixs), 1)
+    children[1,operators_ixs[ix]] = parents[2,operators_ixs[ix]]
+    children[2,operators_ixs[ix]] = parents[1,operators_ixs[ix]]
+  }
+  
   print("STARSI")
   print(numbers_and_operators[parents[1,]])
   print(numbers_and_operators[parents[2,]])
-  children[1,3] <- parents[2,3]
-  children[2,3] <- parents[1,3]
+  #children[1,cxPoints] <- parents[2,cxPoints]
+  #children[2,cxPoints] <- parents[1, cxPoints]
   #
-  print("otric")
+  print("otroci")
   for(i in 1:2){
     print(numbers_and_operators[children[i,]])
   }
@@ -174,18 +125,51 @@ cros <- function(object, parents)
 
 myMutation <- function(object, parent) 
 {
-  return(parent)
+  mutate <- parent <- as.vector(object@population[parent,])
+  numbers_ixs = seq(1, expression_length, by=2)
+  shuffle_ixs = sample(numbers_ixs, num_numbers)
+  
+  mutate[numbers_ixs] = parent[shuffle_ixs]
+  
+  return(mutate)
 }
 
-sel <- function(object){
-  s <- c(1:object@popSize)
-  return (list(population=object@population[s,], fitness=object@fitness[s]))
-}
-
-fit <- function(eq){
-  return (1)
+myRandomSearch <- function() {
+  closest = 100000
+  result = 0
+  equation = 0
+  
+  for (i in 1:1000) {
+    # premesamo stevila in vstavimo v enacbo
+    chosenNum <- sample(c(1:num_numbers),num_numbers)
+    p <- c(1:expression_length)
+    p[seq(1, expression_length, by=2)] <- chosenNum
+    
+    j <- 2
+    for(x in 1:num_operators_in_exp) {
+      opIndex <- sample(c((num_numbers+1):length(numbers_and_operators)),1)
+      
+      p[j] <- opIndex
+      j <- j + 2
+    }
+    str <- paste(numbers_and_operators[p], collapse="")
+    evaluated <- eval(parse(text=str))
+    
+    difference <- abs(goal - evaluated)
+    
+    if (difference < closest){
+      closest = difference
+      result = evaluated
+      equation = p
+    }
+  }
+  print("Najboljši rezultat")
+  print(result)
+  print("enačba")
+  print(numbers_and_operators[equation])
 }
 
 
 # GA <- ga(type <- "permutation", fitness<- function(x){1}, lower <- c1, upper <- 2, popSize <- 50, maxiter <- 5000, run <- 500, pmutation <- 0.2)
-GA1 <- ga(type = "permutation", fitness=fit , lower = 1, upper = 9, population=myInitPopulation, popSize = 20, maxiter = 4, pcrossover = 0.1, crossover = cros, mutation = myMutation)
+GA1 <- ga(type = "permutation", fitness=myFitness , lower = 1, upper = expression_length, population=myInitPopulation, popSize = 30, maxiter = 200, pcrossover = 0.4, pmutation = 0.1, crossover = cros, mutation = myMutation)
+
